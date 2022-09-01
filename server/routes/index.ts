4cd9be9b163292c9ffa333064c2f91f57c8c4580
berglindoma13 <berglindis@gmail.com>
2022-09-01 10:59:01 +0000

    server removed

--- server/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { allRoutes } from './allRoutes';
-
-export const routes = express.Router();
-
-routes.use(allRoutes);
--- web/utils/emailValidation.ts
+++
@@ -1,6 +0,0 @@
-export const validateEmail = (email: string): boolean => {
-  // eslint-disable-next-line max-len
-  // eslint-disable-next-line no-useless-escape
-  const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-  return re.test(String(email).toLowerCase())
-}
