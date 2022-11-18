e7d50a78cac9a6bb58504000fa728eede5e2eb92
berglindoma13 <berglindis@gmail.com>
2022-11-18 13:28:48 +0000

    birgisson next image src

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['byko.is', 'pimdata.boen.com', 'cdn.integrator.is', 'serefni.is','media.sminor.is', 'shelgason.is', "www.fioranese.it"],
+    domains: ['byko.is', 'pimdata.boen.com', 'cdn.integrator.is', 'serefni.is','media.sminor.is', 'shelgason.is', "www.fioranese.it", "www.swisskrono.com", "rooms-floor.com", "www.kahrs.com"],
   },
 }
