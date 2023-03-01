8800a4223bcf785cf9ebcc13189908e88a2014bc
berglindoma13 <berglindis@gmail.com>
2023-03-01 11:56:34 +0800

    type reconfig and more

--- types/products.ts
+++ types/products.ts
@@ -1,32 +1,37 @@
 import { ProductCertificate, CertificateSystem } from './certificates'
 
 export interface ProductProps {
-  id:number
+  id: string
   productid: string,
   title: string,
   description: string,
   shortdescription: string,
   categories: Array<Category>,
   subCategories: Array<Category>,
+  companyid: number
   sellingcompany: Company
   productimageurl: string,
   url: string,
   brand: string,
-  fscUrl: string,
-  epdUrl: string,
-  vocUrl: string,
-  ceUrl: string,
   certificates: Array<ProductCertificate>
   certificateSystems?: Array<CertificateSystem>
+  approved: boolean
 }
 
 export interface Company{
-  id : number,
-  name : string,
-  websiteurl: string,
+  id : number
+  name : string
+  websiteurl: string
+  contact: string
 }
 
 export interface Category{
   id: number
   name: string
+}
+
+export interface SubCategory {
+  id: number
+  name: string
+  parentCategoryName: string
 }
