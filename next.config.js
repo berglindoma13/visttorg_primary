a813a33ef6b9de46617490b7457942f0ceee2256
berglindoma13 <berglindis@gmail.com>
2023-04-20 16:56:20 +0000

    golfefnabudin nextjs images url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['byko.is', 'pimdata.boen.com', 'cdn.integrator.is', 'serefni.is','media.sminor.is', 'shelgason.is', "www.fioranese.it", "www.swisskrono.com", "rooms-floor.com", "www.kahrs.com", "www.sopro.com", "birgisson.is", "proxy-tessa.swikr-web51.eimed-server.de"],
+    domains: ['byko.is', 'pimdata.boen.com', 'cdn.integrator.is', 'serefni.is','media.sminor.is', 'shelgason.is', "www.fioranese.it", "www.swisskrono.com", "rooms-floor.com", "www.kahrs.com", "www.sopro.com", "birgisson.is", "proxy-tessa.swikr-web51.eimed-server.de", "oneflor-europe.be", "golfefnabudin.is", "pim.decora.pl", "www.infloor-girloon.de"],
   },
 }
